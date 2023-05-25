0bb52342d4de3f86b1f9435a929087fdbbda07b9
viza <avizaanandya12@gmail.com>
2023-05-25 21:39:21 +0700

    landing progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import Header from "./component/Header"
+import playIcon from './assets/landing/Play.png'
+import star from './assets/landing/Star.png'
+import landingImg from './assets/landing/landing-img.jpg'
+import underline from './assets/landing/underline.png'
 import './App.scss'
 function App() {
 
   return (
     <>
       <Header/>
+      <section className="landing">
+        <div className="landing-info">
+          <h1 className="landing-title">
+            Everything you need to scale a <span className="landing-title--underline">global team <img src={underline} alt="" /> </span>
+          </h1>
+          <p className="paragraph">
+          Check out our proven methods, guides, and exercises that help make work better, and people happier.
+          </p>
+          <div className="landing-btns">
+            <button className="btn btn--blue">Learn more</button>
+            <button className="btn btn--white btn-w-icon">
+              <img src={playIcon} alt="play-icon" className="play-icon" />
+              <p>Watch demo</p> 
+            </button>
+          </div>
+          <p className="paragraph paragraph--size">Trusted by over than 1M customers</p>
+          <div className="stars">
+            <img className="star" src={star} alt="" />
+            <img className="star" src={star} alt="" />
+            <img className="star" src={star} alt="" />
+            <img className="star" src={star} alt="" />
+            <img className="star" src={star} alt="" />
+          </div>
+        </div>
+        <div className="landing-img">
+          <img src={landingImg} alt="" />
+        </div>
+      </section>
     </>
   )
 }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,26 +3,29 @@ import logoText from '../assets/header/logoText.png'
 import menu from '../assets/header/menu.png'
 
 function Header(){
+    const navLists = ['Home','Features','About Us','News','Contact'];
     return(
         <>
-            <header className="header-wrapper">
-                <a className="header-logo">
-                    <img className='header-logo-img' src={logo} alt="logo" />
-                    <img className='header-logo-text' src={logoText} alt="logotext" />
-                </a>
-                <div className="header-navbar-btn">  
-                    <button className='header-btn'><p>Start Now</p></button>
-                    <div className="header-navbar">
-                        <button className='header-nav-btn'><img src={menu} alt="menu-btn" /></button>
-                        <nav>
-                            <ul className='header-nav-lists'>
-                                <li className='header-nav-list'><a href="#">Home</a></li>
-                                <li className='header-nav-list'><a href="#">Features</a></li>
-                                <li className='header-nav-list'><a href="#">About Us</a></li>
-                                <li className='header-nav-list'><a href="#">News</a></li>
-                                <li className='header-nav-list'><a href="#">Contact</a></li>
-                            </ul>
-                        </nav>
+            <header className='header-section'>
+                <div className="header-wrapper">
+                    <a className="header-logo">
+                        <img className='header-logo-img' src={logo} alt="logo" />
+                        <img className='header-logo-text' src={logoText} alt="logotext" />
+                    </a>
+                    <div className="header-navbar-btn">  
+                        <button className='header-btn'><p>Start Now</p></button>
+                        <div className="header-navbar">
+                            <button className='header-nav-btn'><img src={menu} alt="menu-btn" /></button>
+                            <nav>
+                                <ul className='header-nav-lists'>
+                                    {navLists.map((navList,index)=>{
+                                        return(
+                                            <li key={index} className='header-nav-list'><a href="#">{navList}</a></li>
+                                        )
+                                    })}
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </header>
